6ef15a917e24ad8f9c2d691fd53a63f1569974bb
Durukar <nightdavila@gmail.com>
2025-02-23 01:53:54 -0300

    Creation.

---
+++ vite-raining-letters/src/App.tsx
@@ -0,0 +1,12 @@
+import RainingLetters from "./components/RainingLetters"
+
+function App() {
+  return (
+    <main className="min-h-screen">
+      <RainingLetters />
+    </main>
+  )
+}
+
+export default App
+
